faa4b22ba75337c4efdfe7401a014850eb8496e3
ahmedfog <mr.elkhateb@gmail.com>
2019-12-22 10:49:39 +0200

    Update py3fix.sh

--- py3fix.sh
+++ py3fix.sh
@@ -7,9 +7,10 @@ apt-get install lolcat -y
 apt-get install toilet -y
 echo Python3 FIX By MR ELKHATEB | lolcat&& figlet ABOMAGDY
 apt-get install -y build-essential && apt-get install -y checkinstall && apt-get install -y libreadline-gplv2-dev && apt-get install -y libncursesw5-dev && apt-get install -y libssl-dev && apt-get install -y libsqlite3-dev && apt-get install -y tk-dev && apt-get install -y libgdbm-dev && apt-get install -y libc6-dev && apt-get install -y libbz2-dev && apt-get install -y zlib1g-dev && apt-get install -y openssl && apt-get install -y libffi-dev && apt-get install -y python3-dev && apt-get install -y python3-setuptools && apt-get install -y wget
-mkdir /tmp/Python37
-wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
-tar xvf Python-3.7.2.tar.xz
-cd /tmp/Python37/Python-3.7.2
-./configure
-sudo make altinstall 
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
+sudo tar xzf Python-3.7.4.tgz
+cd Python-3.7.4
+sudo ./configure --enable-optimizations
+sudo make altinstall
+echo la tachel hm ya sasa | lolcat&& figlet ABOMAGDY
